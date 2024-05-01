360e4c5588611eae1eacbeee69f464bed91f2eb6
kankangain <86007975+kankangain@users.noreply.github.com>
2024-05-01 17:33:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var aText = new Array(
+    " Hi! I'm Kankan Gain a student of BCA in Techno India College of Technology. Currently I'm completing my 1st year. I have some interest in gfx, vfx and animation. I also know some fundamental of Cyber Security."
+);
+var iSpeed = 100;
+var iIndex = 0;
+var iArrLength = aText[0].length;
+var iScrollAt = 20;
+
+var iTextPos = 0;
+var sContents = '';
+var iRow;
+
+function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("typedtext");
+
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+        if (iIndex != aText.length) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 500);
+        }
+    } else {
+        setTimeout("typewriter()", iSpeed);
+    }
+}
+
+
+typewriter();
+
+
+let popup = document.getElementById("popup");
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
